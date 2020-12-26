d25bfbe1c3d19b7f5c910635b10738860c66fd8f
Hartmut Schorrig <hartmut@vishia.org>
2020-12-27 00:35:57 +0100

    InspcTargetProxy for TMS320, refl.bin corrig.

--- IDE/VS15_InspcTargetProxy/InspcTargetProxy_ExmplCtrl.sh
+++ IDE/VS15_InspcTargetProxy/InspcTargetProxy_ExmplCtrl.sh
@@ -1,1 +1,5 @@
--refl:D:\vishia\emc\Test_emC\src\test\cpp\emC_Exmpl_Ctrl\genRefl\emC_Exmpl_Ctrl.refl.bin -idsh:emC_Exmpl_TestCtrl
+##-refl:D:\vishia\emc\Test_emC\src\test\cpp\emC_Exmpl_Ctrl\genRefl\emC_Exmpl_Ctrl.refl.bin -idsh:emC_Exmpl_TestCtrl
+-refl:D:/TUI/EmbdPraktikum/TUI-ET-EmbdPract2A/src/main/cpp/MotorCtrl/genRefl/MotorCtrl.refl.bin -idsh:emC_Exmpl_TestCtrl
+
+
+
