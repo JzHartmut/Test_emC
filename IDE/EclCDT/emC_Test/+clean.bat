cd489109c3377a438837402dab522e2f55cc3200
Hartmut Schorrig <hartmut@vishia.org>
2021-04-23 21:15:07 +0200

    IDE/EclCDT/emC_Test yet newly added, rename has failed (handling error)

---
+++ IDE/EclCDT/emC_Test/+clean.bat
@@ -0,0 +1,3 @@
+echo removes %~d0%~p0\Debug
+if exist %~d0%~p0\Debug rmdir /S/Q %~d0%~p0\Debug
+
---
+++ IDE/EclCDT/emC_Test/+cleanRamdisk.bat
@@ -0,0 +1,8 @@
+set DIRDBG=%TMP%\EclCDT\emC_Test\Debug
+if exist Debug rmdir /S/Q Debug
+if exist Debug del Debug 
+
+if exist %DIRDBG% rmdir /S/Q %DIRDBG% 
+mkdir %DIRDBG%
+mklink /J Debug %DIRDBG%
+
